33831f29516b1fbd2a8660eb56a937f4b0a7ac43
Div Loop <80190976+thedivloop@users.noreply.github.com>
2025-04-11 09:27:37 +0200

    Remove ignored files from version control

--- common/constants.py
+++
@@ -1,9 +0,0 @@
-from django.urls import reverse
-
-LOGIN_URL = reverse('users:login')
-REGISTER_URL = reverse('users:register')
-DASHBOARD_URL = reverse('users:dashboard')
-LOGOUT_URL = reverse('users:logout')
-
-LOGIN_TEMPLATE = 'users/login.html'
-REGISTER_TEMPLATE = 'users/register.html'
--- public_frontend/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PublicFrontendConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'public_frontend'
